c874fe0296360922945a0e41b354b01533d8e79b
Choi Gaeun <119295401+eunii2@users.noreply.github.com>
2024-08-02 18:36:08 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 240802/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -2,11 +2,31 @@
 #include <algorithm>
 using namespace std;
 
+/*int gcd(int n, int m){
+    while(m != 0){
+        int temp = n % m;
+        n = m;
+        m = temp;
+    }
+    return n;
+}
+
 int main(){
     int n, m;
     cin >> n >> m;
 
-    int res = __gcd(n, m);
+    int res = gcd(n, m);
+
+    cout << res << '\n';
+
+    return 0;
+}*/
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    int res = __gcd(n, m); // 알고리즘 헤더 안에 최대공약수를 구하는 함수가 존재함
 
     cout << res << '\n';
 
