a9c93694eeee62c945dc9501c06eae0a7ea909ad
Choi Gaeun <119295401+eunii2@users.noreply.github.com>
2024-08-06 20:21:33 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240806/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <numeric>
+#include <numeric> //최소공배수(lcm)을 구하는 함수가 내장되어 있음
 using namespace std;
 
 int main() {
     int n, m;
     cin >> n >> m;
 
-    int res = lcm(n, m);
+    int res = lcm(n, m); //lcm 함수
 
     cout << res;
     return 0;
